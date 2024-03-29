11f973a13f8e2ceb53d72b46d3c7311fb6ffb3ce
Panupong Jindarat <121282153+panupong1212312121@users.noreply.github.com>
2024-03-29 17:45:22 +0700

    improve python code

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
